1ea53abac2d9f1cd70c88af27222e791c6c83ba3
Phillip Badenhorst <phillbaden@gmail.com>
2019-06-14 09:14:42 +0200

    Initial commit, around 50% completion of course

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+//import 'package:flutter/rendering.dart ';
+import 'package:scoped_model/scoped_model.dart';
+
+import 'package:flutter_course/pages/auth.dart';
+import 'package:flutter_course/pages/product_admin.dart';
+import 'package:flutter_course/pages/products.dart';
+import 'package:flutter_course/pages/product.dart';
+import 'package:flutter_course/models/product.dart';
+import 'package:flutter_course/scoped_models/products.dart';
+
+void main() {
+//  debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<ProductsModel>(
+      model: ProductsModel(),
+      child: MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple,
+          buttonColor: Colors.deepPurple,
+        ),
+//      home: AuthPage(),
+        routes: {
+          '/': (BuildContext context) => AuthPage(),
+          '/products': (BuildContext context) => ProductsPage(),
+          '/admin': (BuildContext context) =>
+              ProductsAdminPage(),
+        },
+        onGenerateRoute: (RouteSettings settings) {
+          final List<String> pathElements = settings.name.split('/');
+          if (pathElements[0] != '') {
+            return null;
+          }
+          if (pathElements[1] == 'product') {
+            final int index = int.parse(pathElements[2]);
+            return MaterialPageRoute<bool>(
+              builder: (BuildContext context) => ProductPage(index),
+            );
+          }
+          return null;
+        },
+        onUnknownRoute: (RouteSettings settings) {
+          return MaterialPageRoute(
+              builder: (BuildContext context) => ProductsPage());
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widgets/products/products.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:scoped_model/scoped_model.dart';
+
+import 'package:flutter_course/widgets/products/product_card.dart';
+import 'package:flutter_course/models/product.dart';
+import 'package:flutter_course/scoped_models/products.dart';
+
+class Products extends StatelessWidget {
+  Widget _buildProductList(List<Product> products) {
+    Widget productCard;
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: (BuildContext context, int index) =>
+            ProductCard(products[index], index),
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Center(
+        child: Text('No Products found, please add some.'),
+      );
+    }
+    return productCard;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModelDescendant<ProductsModel>(builder: (BuildContext context, Widget child, ProductsModel model) {
+      return _buildProductList(model.displayProducts);
+    },);
+  }
+}
