c23f897f38a26f5ee85402fddb93f1176c8e755a
Phillip Badenhorst <phillbaden@gmail.com>
2019-07-04 08:29:32 +0200

    Error with saving new product, likely due to API key. Finished section on adding Google Maps, however current lectures have been moved to legacy in course.

--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:async/async.dart';
 
 import 'package:map_view/map_view.dart';
 import 'package:http/http.dart' as http;
+import 'package:location/location.dart' as geoloc;
 
 import '../helpers/ensure_visible.dart';
 import '../../models/location_data.dart';
@@ -30,7 +32,7 @@ class _LocationInputState extends State<LocationInput> {
   void initState() {
     _addressInputFocusNode.addListener(_updateLocation);
     if (widget.product != null) {
-      getStaticMap(widget.product.location.address, false);
+      _getStaticMap(widget.product.location.address, geocode: false);
     }
     super.initState();
   }
@@ -41,7 +43,7 @@ class _LocationInputState extends State<LocationInput> {
     super.dispose();
   }
 
-  void getStaticMap(String address, [geocode = true]) async {
+  void _getStaticMap(String address, {bool geocode = true, double lat, double lng}) async {
     if (address.isEmpty) {
       setState(() {
         _staticMapUri = null;
@@ -53,7 +55,7 @@ class _LocationInputState extends State<LocationInput> {
       final Uri uri = Uri.https(
         'maps.googleapis.com',
         '/maps/api/geocode/json',
-        {'address': address, 'key': 'AIzaSyCLQTG59usHzrIRrkQwmb8Pzu8OMqsa7ho'},
+        {'address': address, 'key': 'AIzaSyCKVTAKTl39S_ZLnq5ByL1--ayuYEBkNIE'},
       );
       final http.Response response = await http.get(uri);
       final decodedResponse = json.decode(response.body);
@@ -64,12 +66,15 @@ class _LocationInputState extends State<LocationInput> {
           address: formattedAddress,
           latitude: coords['lat'],
           longitude: coords['lng']);
-    } else {
+    } else if (lat == null && lng == null) {
       _locationData = widget.product.location;
+    } else {
+      _locationData = LocationData(address: address, latitude: lat, longitude: lng);
     }
 
+    if (mounted) {
     final StaticMapProvider staticMapViewProvider =
-    StaticMapProvider('AIzaSyCLQTG59usHzrIRrkQwmb8Pzu8OMqsa7ho');
+    StaticMapProvider('AIzaSyCKVTAKTl39S_ZLnq5ByL1--ayuYEBkNIE');
     final Uri staticMapUri = staticMapViewProvider.getStaticUriWithMarkers([
       Marker('position', 'Position', _locationData.latitude,
           _locationData.longitude)
@@ -79,15 +84,36 @@ class _LocationInputState extends State<LocationInput> {
         height: 300,
         maptype: StaticMapViewType.roadmap);
     widget.setLocation(_locationData);
-    setState(() {
-      _addressInputController.text = _locationData.address;
-      _staticMapUri = staticMapUri;
-    });
+
+      setState(() {
+        _addressInputController.text = _locationData.address;
+        _staticMapUri = staticMapUri;
+      });
+    }
+  }
+
+  Future<String> _getAddress(double lat, double lng) async {
+    final uri = Uri.https(
+      'maps.googleapis.com',
+      '/maps/api/geocode/json',
+      {'latlng': '${lat.toString()},${lng.toString()}', 'key': 'AIzaSyCKVTAKTl39S_ZLnq5ByL1--ayuYEBkNIE'},
+    );
+    final http.Response response = await http.get(uri);
+    final decodedResponse = json.decode(response.body);
+    final formattedAddress = decodedResponse['results'][0]['formatted_address'];
+    return formattedAddress;
+  }
+
+  void _getUserLocation() async {
+      final location = geoloc.Location();
+      final currentLocation = await location.getLocation();
+      final address = await _getAddress(currentLocation.latitude, currentLocation.longitude);
+      _getStaticMap(address, geocode: false, lat: currentLocation.latitude, lng: currentLocation.longitude);
   }
 
   void _updateLocation() {
     if (!_addressInputFocusNode.hasFocus) {
-      getStaticMap(_addressInputController.text);
+      _getStaticMap(_addressInputController.text);
     }
   }
 
@@ -108,6 +134,14 @@ class _LocationInputState extends State<LocationInput> {
             decoration: InputDecoration(labelText: 'Address'),
           ),
         ),
+        SizedBox(
+          height: 10.0,
+
+        ),
+        FlatButton(child: Text('Locate User'),
+        onPressed: () {
+          _getUserLocation;
+        },),
         SizedBox(
           height: 10.0,
         ),
